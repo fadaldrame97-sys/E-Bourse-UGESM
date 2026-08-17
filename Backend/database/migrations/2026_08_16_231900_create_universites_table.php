3b8ca41e9876204e8fe3704392ec74259b77c05e
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-17 21:52:07 +0100

    Créer les clonnes de la table universites le rendre cohérant avec ERD

--- Backend/database/migrations/2026_08_16_231900_create_universites_table.php
+++ Backend/database/migrations/2026_08_16_231900_create_universites_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('universites', function (Blueprint $table) {
             $table->id();
+            $table->string('nom')->unique();
+            $table->string('ville');
             $table->timestamps();
         });
     }

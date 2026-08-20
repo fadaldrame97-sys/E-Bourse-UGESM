a719358f8bef8259bd02890fad3022cdab135d31
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-21 00:21:17 +0100

    J'ai terminé le model Etudiant avec toutes les relations qu'il faut

--- Backend/app/Models/Etudiant.php
+++ Backend/app/Models/Etudiant.php
@@ -6,5 +6,52 @@ use Illuminate\Database\Eloquent\Model;
 
 class Etudiant extends Model
 {
-    //
+    
+protected $fillable=[
+    'user_id',
+    'universite_id',
+    'matricule',
+    'numero_passeport',
+    'date_naissance',
+    'niveau_etude',
+    'annee_arrivee',
+    'nombre_redoublements',
+    'statut_bourse',
+    'motif_suspension',
+    'duree_formation_annees',
+];
+
+protected function casts(): array
+{
+    return [
+        'date_naissance' => 'date',
+    ];
+}
+
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function universite()
+{
+    return $this->belongsTo(Universite::class);
+}
+
+public function demandeBourses()
+{
+    return $this->hasMany(DemandeBourse::class);
+}
+
+public function billetRetours()
+{
+    return $this->hasMany(BilletRetour::class);
+}
+
+public function notifications()
+{
+    return $this->hasMany(Notification::class);
+}
+
 }

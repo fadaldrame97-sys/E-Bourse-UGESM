48ba0f749d9d4fd93356b6e7a3090e8897e0f3d0
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-21 02:35:48 +0100

    compléter le model document

--- Backend/app/Models/Document.php
+++ Backend/app/Models/Document.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    protected $fillable = [
+    'demande_bourse_id',
+    'nom',
+    'type',
+    'chemin_fichier',
+    'statut_validation',
+];
+
+public function demandeBourse()
+{
+    return $this->belongsTo(DemandeBourse::class);
+}
 }

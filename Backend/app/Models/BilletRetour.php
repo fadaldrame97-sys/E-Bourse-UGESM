f69d246ab54c19a2c958eb358952b75041fd379b
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-21 02:37:08 +0100

    Passer les attributs dans BilletRetour

--- Backend/app/Models/BilletRetour.php
+++ Backend/app/Models/BilletRetour.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class BilletRetour extends Model
 {
-    
+    protected $fillable = [
+    'etudiant_id',
+    'motif',
+    'type',
+    'date_demande',
+    'date_validation',
+    'statut',
+];
 }

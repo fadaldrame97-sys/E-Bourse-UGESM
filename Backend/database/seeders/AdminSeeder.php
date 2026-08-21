42f5d8e7d9dc0dc587b0551d53bef3f589d90802
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-21 22:14:59 +0100

    Créer trois admin de diffrénte type

--- Backend/database/seeders/AdminSeeder.php
+++ Backend/database/seeders/AdminSeeder.php
@@ -2,16 +2,54 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $admins = [
+            [
+                'nom' => 'Ndiaye',
+                'prenom' => 'Super',
+                'email' => 'superadmin@ebourse.sn',
+                'code_agent' => 'ADM-001',
+                'type_admin' => 'super_admin',
+            ],
+            [
+                'nom' => 'Fall',
+                'prenom' => 'Gestionnaire',
+                'email' => 'gestionnaire@ebourse.sn',
+                'code_agent' => 'ADM-002',
+                'type_admin' => 'gestionnaire',
+            ],
+            [
+                'nom' => 'Diop',
+                'prenom' => 'Validateur',
+                'email' => 'validateur@ebourse.sn',
+                'code_agent' => 'ADM-003',
+                'type_admin' => 'validateur',
+            ],
+        ];
+
+        foreach ($admins as $data) {
+            $user = User::create([
+                'nom' => $data['nom'],
+                'prenom' => $data['prenom'],
+                'email' => $data['email'],
+                'password' => Hash::make('password'),
+                'telephone' => '+221 77 8000000',
+                'role' => 'admin',
+            ]);
+
+            Admin::create([
+                'user_id' => $user->id,
+                'code_agent' => $data['code_agent'],
+                'type_admin' => $data['type_admin'],
+            ]);
+        }
     }
 }

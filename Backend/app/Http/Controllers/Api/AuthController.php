0e5585dc68d5094d584968d0b8e79c6ba1f77e90
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-22 00:43:46 +0100

    Créer l'attribute et le constructeur pour AuthService

--- Backend/app/Http/Controllers/Api/AuthController.php
+++ Backend/app/Http/Controllers/Api/AuthController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use App\Http\Requests\Auth\LoginRequest;
 
 use Illuminate\Http\Request;
+use App\Services\AuthService;
+use Illuminate\Routing\Controller;
 
-class AuthController
+class AuthController extends Controller
 {
-    //
+    protected $authService;
+
+    public function __construct(AuthService $authService){
+         $this->authService = $authService;
+
+    }
 }

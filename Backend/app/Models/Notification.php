19d93dabc8c6049020035bca8ccb0e7a5e99b1f0
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-21 02:48:33 +0100

    Dans la methode référence je vais viser l'objet concerné selon la valuer

--- Backend/app/Models/Notification.php
+++ Backend/app/Models/Notification.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+
+protected $fillable = [
+    'etudiant_id',
+    'message',
+    'date_envoi',
+    'type',
+    'reference_id',
+];
+    protected function casts(): array
+{
+    return [
+        'date_envoi' => 'datetime',
+    ];
+}
+
+public function reference()
+{
+    if ($this->type === 'demande_bourse') {
+        return DemandeBourse::find($this->reference_id);
+    }
+
+    if ($this->type === 'billet_retour') {
+        return BilletRetour::find($this->reference_id);
+    }
+
+    return null;
+}
 }

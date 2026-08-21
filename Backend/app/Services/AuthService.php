d7c253c467a9bc8612ec2f39ad5d7a8d6a8fb2a9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-21 23:51:58 +0100

    Requette Eloquete pour récupérer le user depuis la base de donneés

--- Backend/app/Services/AuthService.php
+++ Backend/app/Services/AuthService.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthService{
-    
+
+   public function login(string $email, string $password){
+
+      $user=User::where('email',$email)->first();
+
+      
+
+   }
 
 }

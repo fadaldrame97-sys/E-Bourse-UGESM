c47fe2aae54070b3c792802f09dd74d7c9ece845
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-07 10:45:34 +0100

    ajouter la route pour demander bourse

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DemandeBourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/demandes-bourse', [DemandeBourseController::class, 'store']);
 });

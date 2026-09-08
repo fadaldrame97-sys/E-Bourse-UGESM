c28ce21a4289e66df5406302ba32ee5e5127dba8
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-08 15:58:14 +0100

    Petit contenue pour tester l'affichage de dashboardEtudiant

---
+++ Frontend/src/pages/DashboardEtudiant.jsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+  return (
+    <div className="min-h-screen bg-[#FDF6F3] flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Bienvenue dans votre espace 🎓
+      </h1>
+    </div>
+  );
+}
+
+export default Dashboard;

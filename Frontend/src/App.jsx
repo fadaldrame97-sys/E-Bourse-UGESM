46e9846d8e1f2ef36268be9138d9ca510de67457
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-08 15:23:20 +0100

    importer la page d'accueil et login dans app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,7 @@
-import Accueil from './pages/Accueil';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Accueil from "./pages/Accueil";
+import Login from "./pages/Login";
+
 
 function App() {
   return <Accueil />;

66091636d4f5eafa3c6db005baed3beff854b15f
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-08 15:24:27 +0100

    Mettre le router pour les deux pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,9 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Accueil from "./pages/Accueil";
 import Login from "./pages/Login";
 
-
 function App() {
-  return <Accueil />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

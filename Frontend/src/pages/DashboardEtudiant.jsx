5a477b3444f3e419be2748fb0d73426b97e1c1d6
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-10 13:05:59 +0100

    Importer les Hooks dans Dashboard admine

--- Frontend/src/pages/DashboardEtudiant.jsx
+++ Frontend/src/pages/DashboardEtudiant.jsx
@@ -1,11 +1,2 @@
-function Dashboard() {
-  return (
-    <div className="min-h-screen bg-[#FDF6F3] flex items-center justify-center">
-      <h1 className="text-3xl font-bold">
-        Bienvenue dans votre espace 🎓
-      </h1>
-    </div>
-  );
-}
-
-export default Dashboard;
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";

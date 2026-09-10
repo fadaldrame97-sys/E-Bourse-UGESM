0497a88b51a896c8cfb740cb5695585d55d9a815
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-10 13:09:54 +0100

    Mettre en place l'état de l'utilisateur

--- Frontend/src/pages/DashboardEtudiant.jsx
+++ Frontend/src/pages/DashboardEtudiant.jsx
@@ -1,3 +1,8 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
+
+function DashboardEtudiant(){
+
+  const [utilisateur, setUtilisateur]=useState(null);
+}

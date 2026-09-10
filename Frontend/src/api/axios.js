39e23ecc2f8b02ddea17816ba3569b17d763cf32
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-10 13:02:50 +0100

    Récupérer authomatique des token pour chaque requette

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,6 +3,13 @@ import axios from "axios";
 const api=axios.create({
 
     baseURL:'http://127.0.0.1:8000/api',
-})
+});
+
+api.interceptors.request.use(function (config) {
+    const token=localStorage.getItem('token');
+
+    if (token) config.headers.Authorization='Bear'+ token;
+    return config;
+});
 
 export default api;

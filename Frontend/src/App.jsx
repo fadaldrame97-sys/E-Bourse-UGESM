069fe2380cfc44c4dc83cad167c79d7b85abe568
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 16:14:16 +0100

    import le composant et créer la route de MesDemandes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Accueil from "./pages/Accueil";
 import Login from "./pages/Login";
 import DashboardEtudiant from "./pages/DashboardEtudiant";
 import DemandeBourse from "./components/DemandeBourse";
+import MesDemandes from "./pages/MesDemandes";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<DashboardEtudiant />} />
         <Route path="/nouvelle-demande" element={<DemandeBourse />} />
+        <Route path="/mes-demandes" element={<MesDemandes />} />
       </Routes>
     </BrowserRouter>
   );

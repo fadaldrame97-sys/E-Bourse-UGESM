9cb6a42eb3386b1291fdee2496cc6d49367a4989
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 12:18:54 +0100

    passer la requette de la demande

--- Frontend/src/components/DemandeBourse.jsx
+++ Frontend/src/components/DemandeBourse.jsx
@@ -11,4 +11,29 @@ function DemandeBourse(){
 
     const navigate = useNavigate();
 
+
+
+    function gererDemande(e) {
+    e.preventDefault();
+
+    setErreur('');
+    setSucces('');
+    setChargement(true);
+
+    api.post('/demandes-bourse', { type: type })
+      .then(function (reponse) {
+       
+        setSucces('Demande créée avec succès ! Numéro de dossier : ' + reponse.data.demande.numero_dossier);
+      })
+      .catch(function (error) {
+        setChargement(false);
+
+        if (error.response && error.response.data && error.response.data.message) {
+          setErreur(error.response.data.message);
+        } else {
+          setErreur('Une erreur est survenue. Réessayez.');
+        }
+      });
+  }
+
 }

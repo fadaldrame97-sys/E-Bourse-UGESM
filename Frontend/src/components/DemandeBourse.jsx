23eb5aa3bb73796e0609aa06c770aabbcddd72a6
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 12:11:06 +0100

    Stocker le useNavigate

--- Frontend/src/components/DemandeBourse.jsx
+++ Frontend/src/components/DemandeBourse.jsx
@@ -4,6 +4,11 @@ import api from "../api/axios";
 
 function DemandeBourse(){
 
-     const [type, setType] = useState('premiere_attribution');
+    const [type, setType] = useState('premiere_attribution');
+    const [erreur, setErreur] = useState('');
+    const [succes, setSucces] = useState('');
+
+
+    const navigate = useNavigate();
 
 }

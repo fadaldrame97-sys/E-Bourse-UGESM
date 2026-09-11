dcc980bd6b000aad5a9677c5d7813080135212fd
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 16:16:46 +0100

    ajouter une button qui mène vers MesDemandes

--- Frontend/src/pages/DashboardEtudiant.jsx
+++ Frontend/src/pages/DashboardEtudiant.jsx
@@ -88,6 +88,13 @@ function DashboardEtudiant(){
               Nouvelle demande de bourse
           </button>
 
+          <button
+                onClick={function () { navigate('/mes-demandes'); }}
+                className="w-full mt-2 border border-[#D3D1C7] rounded-lg py-2.5 text-sm font-semibold text-[#2C2C2A]"
+              >
+                Voir mes demandes
+          </button>
+
           <div className="grid grid-cols-2 gap-4 text-sm">
             <div>
               <p className="text-[#888780]">Matricule</p>

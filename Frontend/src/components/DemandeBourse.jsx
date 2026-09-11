7e107cf938de296566405f99fadffdbcfa7b08df
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 12:09:05 +0100

    Initialiser le state de la demande 'première_attribution par defaut'

--- Frontend/src/components/DemandeBourse.jsx
+++ Frontend/src/components/DemandeBourse.jsx
@@ -3,5 +3,7 @@ import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
 
 function DemandeBourse(){
-    
+
+     const [type, setType] = useState('premiere_attribution');
+
 }

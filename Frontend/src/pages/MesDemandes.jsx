434a1ba6f587f37f1ba79d643c9834c5e6f85ab8
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 15:54:57 +0100

    Styler les données selon la valeur

--- Frontend/src/pages/MesDemandes.jsx
+++ Frontend/src/pages/MesDemandes.jsx
@@ -26,4 +26,26 @@ function MesDemandes(){
 
    },[]);
 
+
+
+    function couleurBadge(statut) {
+    if (statut === 'validee') {
+      return 'bg-[#EAF3DE] text-[#27500A]';
+    } else if (statut === 'rejetee') {
+      return 'bg-red-100 text-red-700';
+    } else if (statut === 'incomplet') {
+      return 'bg-orange-100 text-orange-700';
+    } else {
+      return 'bg-gray-100 text-gray-700';
+    }
+  }
+
+  function libelleType(type) {
+    if (type === 'premiere_attribution') {
+      return 'Première attribution';
+    } else {
+      return 'Renouvellement';
+    }
+  }
+
 }

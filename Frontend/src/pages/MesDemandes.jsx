c01769200095446876df4a4ee47fc32d3aa07e34
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 15:35:35 +0100

    importer les composants et crée une function MesDemandes

---
+++ Frontend/src/pages/MesDemandes.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/axios";
+import Carte from "../components/Carte";
+
+
+function MesDemandes(){
+    
+}

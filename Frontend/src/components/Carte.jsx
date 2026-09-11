76862f43b2ae3645e2eb0e6a8efa05f0d3c4f1ad
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 15:23:20 +0100

    Créer un composant de style des carte don app unique

---
+++ Frontend/src/components/Carte.jsx
@@ -0,0 +1,9 @@
+function Carte({ children }) {
+  return (
+    <div className="bg-white rounded-2xl shadow-[0_4px_16px_rgba(0,0,0,0.05)] p-6">
+      {children}
+    </div>
+  );
+}
+
+export default Carte;

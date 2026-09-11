c0d02a5a6284213a6ad60d6329b85404b9ead4b9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 15:29:32 +0100

    Importer le composant carte

--- Frontend/src/components/DemandeBourse.jsx
+++ Frontend/src/components/DemandeBourse.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
+import Carte from "../components/Carte";
 
 function DemandeBourse(){
 

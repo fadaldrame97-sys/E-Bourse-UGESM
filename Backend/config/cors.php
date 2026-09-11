f43c76f3fd0dec597929eafc0c908c63fe685517
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-11 10:11:47 +0100

    Corriger le blocage de port coté front et backend

---
+++ Backend/config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [],
+
+    'allowed_origins_patterns' => [
+        '#^http://localhost:[0-9]+$#',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -9,4 +9,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/demandes-bourse', [DemandeBourseController::class, 'store']);
+    Route::get('/me', [AuthController::class, 'me']);
 });

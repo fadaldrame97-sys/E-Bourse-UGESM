1c651ff085870bef65f40304e15df47f39ee48ca
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-14 15:46:22 +0100

    empecher l'étudiant de demander plus d'une fois

--- Backend/app/Services/BilletRetourService.php
+++ Backend/app/Services/BilletRetourService.php
@@ -15,6 +15,16 @@ class BilletRetourService{
         if (!$etudiant) {
             throw new \Exception('Seul un étudiant peut demander un billet retour.');
         }
+
+
+         $billetActif = BilletRetour::where('etudiant_id', $etudiant->id)
+            ->where('statut', 'en_attente')
+            ->first();
+
+        if ($billetActif) {
+            throw new \Exception('Vous avez déjà une demande de billet retour en cours.');
+        }
+
     }
 
 }

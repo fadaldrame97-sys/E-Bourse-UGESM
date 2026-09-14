82625bd36690bcc8a7bcdbabee67c1322bfc4536
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-14 15:43:49 +0100

    Créer une function de create et permettre un étudiant seule de faire une demande

--- Backend/app/Services/BilletRetourService.php
+++ Backend/app/Services/BilletRetourService.php
@@ -6,5 +6,15 @@ use App\Models\BilletRetour;
 use Illuminate\Support\Facades\Auth;
 
 class BilletRetourService{
-    
+
+
+        public function create(array $data){
+        $user = Auth::user();
+        $etudiant = $user->etudiant;
+
+        if (!$etudiant) {
+            throw new \Exception('Seul un étudiant peut demander un billet retour.');
+        }
+    }
+
 }

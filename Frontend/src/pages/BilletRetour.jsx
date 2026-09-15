b0aec526e876c8e0dfa4c2ad5667ed9e684913df
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-15 11:23:33 +0100

    Déclarer l'etat de type

--- Frontend/src/pages/BilletRetour.jsx
+++ Frontend/src/pages/BilletRetour.jsx
@@ -3,3 +3,7 @@ import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
 import Carte from "../components/Carte";
 import BoutonPrimaire from "../components/BoutonPrimaire";
+
+function BilletRetour(){
+
+    const [type, setType]=useState}

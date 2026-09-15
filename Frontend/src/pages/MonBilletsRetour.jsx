30c4f33372f02de7c13c9be9562ca2cb16c1eac9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-15 16:59:02 +0100

    Créer function MonBilletRetour

--- Frontend/src/pages/MonBilletsRetour.jsx
+++ Frontend/src/pages/MonBilletsRetour.jsx
@@ -1,2 +1,28 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+function MonBilletsRetour() {
+
+    const [billets, setBillets] = useState([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+
+        axios.get("/api/billets-retour")
+            .then(response => {
+                setBillets(response.data.billets);
+            })
+            .catch(() => {
+                setError("Impossible de récupérer vos demandes.");
+            })
+           
+
+    }, []);
+
+  
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    }

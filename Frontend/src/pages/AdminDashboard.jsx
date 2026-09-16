4084215d30fb405d3d6a1e9f11589511b823d1cc
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-16 14:57:25 +0100

    passer deux requettes: récupérer les infos du admin connecté et récupérer les statistiques, dans un hook useAffecte

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -11,4 +11,17 @@ function AdminDashboard(){
     
     const navigate=useNavigate()
 
+
+      useEffect(function () {
+    api.get('/me')
+      .then(function (reponse) {
+        setAdmin(reponse.data.user.admin);
+      })
+      .catch(function () {});
+
+    api.get('/admin/statistiques')
+      .then(function (reponse) { setStats(reponse.data); })
+      .catch(function () {});
+  }, []);
+
 }

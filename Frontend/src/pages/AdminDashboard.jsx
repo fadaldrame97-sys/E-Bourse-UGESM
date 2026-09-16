b9429d8c79080dc39385a22efe9d62c67c1db41b
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-16 14:44:53 +0100

    Création du composant AdminDashboard

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -3,3 +3,7 @@ import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
 import Carte from "../components/Carte";
 import NavBar from "../components/NavBar";
+
+function AdminDashboard(){
+    
+}

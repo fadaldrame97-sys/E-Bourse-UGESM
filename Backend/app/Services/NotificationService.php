77baa017f994ee099dd390e9a553e8b824083729
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-17 16:30:17 +0100

    Commencer la partie de création de notification coté backend

---
+++ Backend/app/Services/NotificationService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
+
+class NotificationService
+{
+    public function creer($etudiantId, $message, $type, $referenceId)
+    {
+        return Notification::create([
+            'etudiant_id' => $etudiantId,
+            'message' => $message,
+            'date_envoi' => now(),
+            'type' => $type,
+            'reference_id' => $referenceId,
+        ]);
+    }
+
+     }

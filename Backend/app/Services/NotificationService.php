76ef09f08addfa1cbd833b28913e5f97d5803dad
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-21 13:19:33 +0000

    Créer la function creer pour créer notification

--- Backend/app/Services/NotificationService.php
+++ Backend/app/Services/NotificationService.php
@@ -5,5 +5,11 @@ use App\Models\Notification;
 use Illuminate\Support\Facades\Auth;
 
 class NotificationService{
-    
+
+    public function creer($etudiantId, $message, $type, $referenceId){
+        return Notification::create([
+
+        ]);
+    }
+
 }
